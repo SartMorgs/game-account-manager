4666fef9e95ae99181955f285e3815be879198a7
Morgana Sartor <morgana.sartor@corp.mobicare.com.br>
2022-04-19 12:02:07 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace game_account_manager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
